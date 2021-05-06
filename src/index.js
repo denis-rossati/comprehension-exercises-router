84582d8b8bb85d14204ff003bdf8b1e84be442ab
denis-rossati <denis.rossati@hotmail.com>
2021-05-06 15:59:17 -0300

    1-2 feitos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import App from './App';
+import  About from './components/About';
+import Users from './components/Users';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter >
+      <Route exact path='/' component={ App } />
+      <Route path='/about' component={ About } />
+      <Route path='/users' component={ Users } />
+      <Link to='/'> Home </Link>
+      <Link to='/about'> About </Link>
+      <Link to='/users'> Users </Link>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
